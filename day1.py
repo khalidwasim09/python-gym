fa2374da93d5cf46a5c469edcba1f0159f977ef4
khalidwasim09 <khalidwasimalam@gmail.com>
2025-04-21 18:55:00 -0400

    Task 1 and bonus task

--- day1.py
+++ day1.py
@@ -1,1 +1,14 @@
-print ("Hello Python")
+name = input("What's your name ? ")
+age = int(input("What's your age ? "))
+
+if age >= 18: 
+    print (f"Welcome {name}! You're allowed in.") 
+    city = input("What's your city ? ")
+    
+    if city.lower() == "toronto": 
+        print("Welcome to the 6ix")
+    else: 
+        print(f"Nice to meet someone from {city}")
+
+else: 
+    print(f"Sorry {name}, you're too young")
